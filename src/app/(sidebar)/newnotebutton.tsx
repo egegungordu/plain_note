10284027d39db1c3b4002723a15f6c3af748f45a
Ege Gunordu <ege.gungordu@ozu.edu.tr>
2023-08-18 04:10:10 +0900

    navigate to new note when created

--- src/app/(sidebar)/newnotebutton.tsx
+++ src/app/(sidebar)/newnotebutton.tsx
@@ -3,9 +3,11 @@
 import { TbPencil } from "react-icons/tb"
 import { createNote } from "./serveractions"
 import { useTransition } from 'react'
+import { useRouter } from "next/navigation"
 
 export default function NewNoteButton() {
   const [isPending, startTransition] = useTransition()
+  const router = useRouter();
 
   console.log(isPending)
 
@@ -13,6 +15,8 @@ export default function NewNoteButton() {
     <button
       onClick={() => startTransition(async () => {
         const note = await createNote()
+        if (!note) return
+        router.push(`/note/${note.id}`)
       })}
       className="border-2 border-orange-700 mx-4 mt-4 rounded-full hover:bg-orange-700 transition-colors h-10 flex items-center justify-center">
       <span className="text-sm font-semibold text-neutral-300">
