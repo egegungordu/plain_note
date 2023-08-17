3254e9a6912fad059350cc3d219438bfabac1248
Ege Gunordu <ege.gungordu@ozu.edu.tr>
2023-08-18 04:41:52 +0900

    add loader to new note button

--- src/app/(sidebar)/newnotebutton.tsx
+++ src/app/(sidebar)/newnotebutton.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { TbPencil } from "react-icons/tb"
+import { TbLoader, TbLoader2, TbPencil } from "react-icons/tb"
 import { createNote } from "./serveractions"
 import { useTransition } from 'react'
 import { useRouter } from "next/navigation"
@@ -9,8 +9,6 @@ export default function NewNoteButton() {
   const [isPending, startTransition] = useTransition()
   const router = useRouter();
 
-  console.log(isPending)
-
   return (
     <button
       onClick={() => startTransition(async () => {
@@ -18,11 +16,16 @@ export default function NewNoteButton() {
         if (!note) return
         router.push(`/note/${note.id}`)
       })}
-      className="border-2 border-orange-700 mx-4 mt-4 rounded-full hover:bg-orange-700 transition-colors h-10 flex items-center justify-center">
-      <span className="text-sm font-semibold text-neutral-300">
-        New note
-      </span>
-      <TbPencil className="text-lg text-neutral-300 ml-2" />
+      disabled={isPending}
+      className="border-2 border-orange-700 mx-4 mt-4 rounded-full hover:bg-orange-700 transition-colors h-10 flex items-center justify-center disabled:opacity-70">
+      {isPending ? <TbLoader2 className="text-lg animate-spin text-neutral-300" /> :
+        <>
+          <span className="text-sm font-semibold text-neutral-300">
+            New note
+          </span>
+          <TbPencil className="text-lg text-neutral-300 ml-2" />
+        </>
+      }
     </button>
   )
 }
