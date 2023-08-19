66890a2ca3691458f62e969d1f167af40a90b9bb
Ege Gunordu <ege.gungordu@ozu.edu.tr>
2023-08-20 06:42:33 +0900

    fix bug, placeholder blocking input field

--- src/app/note/[note]/interactivenote.tsx
+++ src/app/note/[note]/interactivenote.tsx
@@ -44,11 +44,11 @@ export default function InteractiveNote({ note }: { note: Note }) {
 
   return (
     <div className="py-8 w-full h-full overflow-auto flex flex-col">
-      <h1 id="note-title" placeholder="Untitled" onKeyDown={handleTitleKeyDown} spellCheck contentEditable suppressContentEditableWarning className="text-3xl px-8 mt-1 font-semibold text-neutral-300 empty:text-neutral-600 focus:before:absolute focus:before:w-0.5 focus:before:h-full focus:before:bg-orange-700 border-l-4 border-l-transparent focus:border-l-orange-700 focus:before:-left-2 relative focus:outline-none empty:after:content-[attr(placeholder)] hyphens-auto break-word" style={{ overflowWrap: "break-word" }}>
+      <h1 id="note-title" placeholder="Untitled" onKeyDown={handleTitleKeyDown} spellCheck contentEditable suppressContentEditableWarning className="text-3xl px-8 mt-1 font-semibold text-neutral-300 empty:text-neutral-600 focus:before:absolute focus:before:w-0.5 focus:before:h-full focus:before:bg-orange-700 border-l-4 border-l-transparent focus:border-l-orange-700 focus:before:-left-2 relative focus:outline-none empty:after:content-[attr(placeholder)] empty:after:pointer-events-none hyphens-auto break-word" style={{ overflowWrap: "break-word" }}>
         {note.title}
       </h1>
 
-      <p id="note-content" ref={contentRef} onInput={handleParagraphInput} placeholder="Simply type here..." spellCheck contentEditable suppressContentEditableWarning className="text-neutral-300 mt-4 px-8 items-center empty:text-neutral-600 focus:before:absolute focus:before:w-0.5 focus:before:h-full focus:before:bg-orange-700 border-l-4 border-l-transparent focus:border-l-orange-700 focus:before:-left-2 relative focus:outline-none empty:after:content-[attr(placeholder)] break-all hyphens-manual">
+      <p id="note-content" ref={contentRef} onInput={handleParagraphInput} placeholder="Simply type here..." spellCheck contentEditable suppressContentEditableWarning className="text-neutral-300 mt-4 px-8 items-center empty:text-neutral-600 focus:before:absolute focus:before:w-0.5 focus:before:h-full focus:before:bg-orange-700 border-l-4 border-l-transparent focus:border-l-orange-700 focus:before:-left-2 relative focus:outline-none empty:after:content-[attr(placeholder)] empty:after:pointer-events-none break-all hyphens-manual">
         {note.content}
       </p>
     </div>
