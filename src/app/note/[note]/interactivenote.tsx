b24f7588a91e44a236bb367ae823b51344d777e7
Ege Gunordu <ege.gungordu@ozu.edu.tr>
2023-08-22 00:16:12 +0900

    make edits persist between page changes

--- src/app/note/[note]/interactivenote.tsx
+++ src/app/note/[note]/interactivenote.tsx
@@ -1,9 +1,9 @@
 "use client"
 
 import { Note } from "@prisma/client"
-import { FormEventHandler, useRef, useTransition } from "react"
-import { useEventListener } from "usehooks-ts"
-
+import { FormEventHandler, useEffect, useLayoutEffect, useRef, useState, useTransition } from "react"
+import { useEffectOnce, useEventListener } from "usehooks-ts"
+import { store } from "@/store"
 import { editStoreNote } from "@/store/notesSlice"
 import { useDispatch } from "react-redux"
 import { AppDispatch } from "@/store"
@@ -13,7 +13,19 @@ const useAppDispatch = () => useDispatch<AppDispatch>()
 export default function InteractiveNote({ note }: { note: Note }) {
   const noteRef = useRef<HTMLDivElement>(null)
   const contentRef = useRef<HTMLParagraphElement>(null)
+  const [editedNote, setEditedNote] = useState(note)
   const dispatch = useAppDispatch()
+
+  useEffectOnce(() => {
+    const editedNote = store.getState().notes.editedNotesBuffer[note.id];
+    if (editedNote) {
+      setEditedNote((prev) => ({
+        ...prev,
+        content: editedNote.content ?? prev.content,
+        title: editedNote.title ?? prev.title,
+      }))
+    }
+  })
 
   const handleTitleKeyDown = (e: React.KeyboardEvent<HTMLHeadingElement>) => {
     if (e.key === "Enter") {
@@ -88,11 +100,11 @@ export default function InteractiveNote({ note }: { note: Note }) {
   return (
     <div ref={noteRef} className="py-8 w-full max-w-full h-full overflow-auto flex flex-col">
       <h1 id="note-title" placeholder="Untitled" onInput={handleTitleInput} onKeyDown={handleTitleKeyDown} spellCheck contentEditable suppressContentEditableWarning className="text-4xl px-8 mt-1 font-semibold text-neutral-300 empty:text-neutral-600 focus:before:absolute focus:before:w-0.5 focus:before:h-full focus:before:bg-orange-700 border-l-4 border-l-transparent focus:border-l-orange-700 focus:before:-left-2 relative focus:outline-none empty:after:content-[attr(placeholder)] empty:after:pointer-events-none hyphens-manual break-all">
-        {note.title}
+        {editedNote.title}
       </h1>
 
       <p id="note-content" ref={contentRef} onInput={handleContentInput} placeholder="Simply type here..." spellCheck contentEditable suppressContentEditableWarning className="text-neutral-300 mt-8 px-8 items-center empty:text-neutral-600 focus:before:absolute focus:before:w-0.5 focus:before:h-full focus:before:bg-orange-700 border-l-4 border-l-transparent focus:border-l-orange-700 focus:before:-left-2 relative focus:outline-none empty:after:content-[attr(placeholder)] empty:after:pointer-events-none break-all hyphens-manual">
-        {note.content}
+        {editedNote.content}
       </p>
     </div>
   )
